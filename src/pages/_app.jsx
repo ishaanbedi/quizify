2b2f7743e66d8e727a5c63b55bd6b9b1c0b11657
Ishaan Bedi <bediishaan6@gmail.com>
2022-08-31 16:20:21 +0530

    Added analytics tracking

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,6 @@
-import { AppProps } from 'next/app';
+import React from 'react';
+import Script from 'next/script';
+
 import NextNProgress from 'nextjs-progressbar';
 
 import '../styles/globals.css';
@@ -6,14 +8,25 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextNProgress
-        color='#fff'
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
-      <Component {...pageProps} />
+      <React.Fragment>
+        <NextNProgress
+          color='#fff'
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+        <Component {...pageProps} />
+        <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
+        <noscript>
+          {/* eslint-disable @next/next/no-img-element */}
+          <img
+            src='https://queue.simpleanalyticscdn.com/noscript.gif'
+            alt=''
+            referrerPolicy='no-referrer-when-downgrade'
+          />
+        </noscript>
+      </React.Fragment>
     </>
   );
 }
